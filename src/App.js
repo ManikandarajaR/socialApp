f48e33dab02e9303cd58cc51b76b3ade75477f86
manikandarajar.20mca@kongu.edu <manikandanraja1204@gmail.com>
2021-08-08 09:02:27 +0530

    CHanges Chaged

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 //import './App.css';
-
-
+import Axios from 'axios';
 import React,{useState} from 'react';
 
 function App() {
 
-  const url = "";
+  const url = "https://webhook.site/1f900d3c-4229-4c58-a301-c115806e4f80";
 
   const [data, setdata] = useState(
     {
@@ -15,21 +14,41 @@ function App() {
     }
   )
 
+  function submit(e)
+  {
+    e.preventDefault();
+
+    Axios.post(url,
+      {
+        name:data.name,
+        lname:data.lname,
+        cirty:data.city
+      })
+      .then(res => {
+        console.log(res.data);
+      })
+
+  }
+
   function handle(e)
   {
-  
+      const newdata = {...data}
+      newdata[e.target.id] = e.target.value;
+      setdata(newdata);
+      console.log(newdata);
   }
 
 
   return (
     <div className="App">
      
-      <form  >
+      <form onSubmit={(e) => submit(e)}  >
           
             <input onChange={(e) => handle(e)}   value = {data.name} placeholder="name" id="name"  type="text" ></input>
             <input onChange={(e) => handle(e)}   value = {data.lname}  placeholder="lname" id="lname" type="text" ></input>
             <input onChange={(e) => handle(e)}  value = {data.city}    placeholder="city" id="city" type="text" ></input>
 
+            <button type="submit">Sumbit</button>
       </form>
 
     </div>
---
+++ src/test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+export default function test() {
+     return (
+          <div>
+
+               
+
+
+
+          </div>
+     )
+}
